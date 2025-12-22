0232c1bbf6281c7a584dda53633e958d91efd699
Prithvi <prithvicv@gmail.com>
2025-12-22 20:47:05 +0530

    Layout and report changes

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -12,6 +12,5 @@ export const appConfig: ApplicationConfig = {
       withHashLocation(),
       withEnabledBlockingInitialNavigation()
     )
-
   ]
 };
--- src/app/pages/layout/layout.ts
+++ src/app/pages/layout/layout.ts
@@ -3,6 +3,7 @@ import { RouterOutlet,RouterLink } from '@angular/router';
 import { LeftSidebar } from './left-sidebar/left-sidebar';
 import { Main } from './main/main';
 import { HeaderComponent } from './header/header.component';
+import { LayoutService } from '../../core/services/layout.service';
 
 @Component({
   selector: 'app-layout',
@@ -11,23 +12,27 @@ import { HeaderComponent } from './header/header.component';
   styleUrl: './layout.css'
 })
 export class Layout implements OnInit {
-isLeftSidebarCollapsed = signal<boolean>(false);
+get isLeftSidebarCollapsed() {
+  return this.layoutService.getIsLeftSidebarCollapsedSignal();
+}
 screenWidth = signal<number>(window.innerWidth);
+
+  constructor(private layoutService: LayoutService) {}
 
   @HostListener('window:resize')
   onResize() {
     this.screenWidth.set(window.innerWidth);
     if(this.screenWidth() < 768) {
-      this.isLeftSidebarCollapsed.set(true);
+      this.layoutService.setIsLeftSidebarCollapsed(true);
     }
   }
 
   ngOnInit(): void {
-    this.isLeftSidebarCollapsed.set(this.screenWidth() < 768);
+    this.layoutService.setIsLeftSidebarCollapsed(this.screenWidth() < 768);
   }
 
  changeIsLeftSidebarCollapsed(isLeftSidebarCollapsed: boolean): void {
-  this.isLeftSidebarCollapsed.set(isLeftSidebarCollapsed);
+  this.layoutService.setIsLeftSidebarCollapsed(isLeftSidebarCollapsed);
 }
 
 }
